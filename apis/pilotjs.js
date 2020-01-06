639e833eefb06cc58b6d67cf8e85253142be42e8
arhoy <you@example.com>
2020-01-06 14:00:30 -0700

    logo updated

--- apis/pilotjs.js
+++ apis/pilotjs.js
@@ -1,16 +1,16 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-// get amazon products from my published API
-exports.getAmazonProducts = async () => {
-  const res = await axios.get(
-    `https://pilotjs.com/api/v1/amazonproducts?limit=500`,
-  );
+// // get amazon products from my published API
+// exports.getAmazonProducts = async () => {
+//   const res = await axios.get(
+//     `https://pilotjs.com/api/v1/amazonproducts?limit=500`,
+//   );
 
-  return res.data;
-};
+//   return res.data;
+// };
 
-exports.getAmazonElectronics = async () => {
-  const res = await axios.get(`https://pilotjs.com/api/v1/amazon-electronics`);
+// exports.getAmazonElectronics = async () => {
+//   const res = await axios.get(`https://pilotjs.com/api/v1/amazon-electronics`);
 
-  return res.data;
-};
+//   return res.data;
+// };
