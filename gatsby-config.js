c1021bb7795c4bed5fc0d70778767ce6045b0d30
arhoy <you@example.com>
2020-01-23 20:56:37 -0700

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,24 +87,24 @@ module.exports = {
     },
     `gatsby-plugin-twitter`,
 
-    // {
-    //   resolve: `gatsby-plugin-stripe`,
-    // },
-    // {
-    //   resolve: `gatsby-source-stripe`,
-    //   options: {
-    //     objects: [
-    //       'Balance',
-    //       'BalanceTransaction',
-    //       'Product',
-    //       'ApplicationFee',
-    //       'Sku',
-    //       'Subscription',
-    //     ],
-    //     secretKey: process.env.STRIPE_SECRET_KEY,
-    //     downloadFiles: true,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-stripe`,
+    },
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: [
+          'Balance',
+          'BalanceTransaction',
+          'Product',
+          'ApplicationFee',
+          'Sku',
+          'Subscription',
+        ],
+        secretKey: process.env.STRIPE_SECRET_KEY,
+        downloadFiles: true,
+      },
+    },
     // {
     //   resolve: 'gatsby-plugin-snipcart',
     //   options: {
